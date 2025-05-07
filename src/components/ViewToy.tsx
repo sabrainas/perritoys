69b4214d9787de52f4680b073be41c348439c0d8
sabrainas <sazonass.m@gmail.com>
2025-05-06 23:42:44 -0300

    adicionando marca

--- src/components/ViewToy.tsx
+++ src/components/ViewToy.tsx
@@ -34,6 +34,7 @@ export default function ViewToy() {
         <div className="flex-1 space-y-4">
           <h2 className="text-gray-600 text-sm">ID: {brinquedo.codigo}</h2>
           <h1 className="text-2xl font-semibold text-gray-800">{brinquedo.descricao}</h1>
+          <h1 className="text-2xl font-semibold text-gray-800">{brinquedo.marca}</h1>
           <p className="text-lg font-bold text-[#c84755]">R$ {brinquedo.valor}</p>
         </div>
       </section>
