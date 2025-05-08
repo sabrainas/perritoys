bb02e0d1e668c5b29bec4d1cdd2877909e00909f
sabrainas <sazonass.m@gmail.com>
2025-05-08 09:52:34 -0300

    adicionando RA

--- src/pages/SobrePage.tsx
+++ src/pages/SobrePage.tsx
@@ -8,11 +8,11 @@ import { UsCard } from '../components/UsCard';
 export function SobrePage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-      <UsCard nome="Eduardo Antonio" ra="123456789" imagem={duduImg} />
+      <UsCard nome="Eduardo Antonio" ra="1670482312008" imagem={duduImg} />
       <UsCard nome="Gustavo Pires" ra="1670482312033" imagem={gustaImg} />
-      <UsCard nome="Johann Benjamin" ra="987654321" imagem={bolivarImg} />
+      <UsCard nome="Johann Benjamin" ra="1670482312014" imagem={bolivarImg} />
       <UsCard nome="Sabrina Mendonça" ra="1670482312046" imagem={euLindaImg} />
-      <UsCard nome="Vinicius Silva" ra="987654321" imagem={cruzImg} />
+      <UsCard nome="Vinicius Silva" ra="1670482312013" imagem={cruzImg} />
     </div>
   );
 }
