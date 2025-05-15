2a4bb1ca239645b7635b03a84effe43852a66ff6
sabrainas <sazonass.m@gmail.com>
2025-05-15 19:44:50 -0300

    ajuste titulo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ export default function BrinquedoPage() {
         <p className="p-4">Carregando brinquedos...</p>
       ) : (
         <>
-          <Header title="Brinquedos em Destaque" />     
+          <Header title="Brinquedos Disponíveis" />     
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4 p-4 ">
             {brinquedos.map((brinquedo) => (
               <BrinquedoCard key={brinquedo.codigo} brinquedo={brinquedo} />
